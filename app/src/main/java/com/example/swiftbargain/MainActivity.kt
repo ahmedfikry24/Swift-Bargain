8bfad4590258b80f4f5af85d3a64b7a13fc82336
Ahmed Fikry <ahmedfikryelshimi24@gmail.com>
2024-02-27 17:17:46 +0200

    switch between splash theme according android version

--- app/src/main/java/com/example/swiftbargain/MainActivity.kt
+++ app/src/main/java/com/example/swiftbargain/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.swiftbargain
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Base_Theme_SwiftBargain)
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    private fun initSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            installSplashScreen()
+        else
+            setTheme(R.style.Base_Theme_SwiftBargain)
     }
 }
